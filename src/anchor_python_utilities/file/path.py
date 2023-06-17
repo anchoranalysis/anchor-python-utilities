9c749ccf8bfd4e0d805f2dd5ab498ea18714c699
Owen Feehan <owenfeehan@users.noreply.github.com>
2023-06-17 08:56:00 +0200

    calling common workflow; linting formatting

--- src/anchor_python_utilities/file/path.py
+++ src/anchor_python_utilities/file/path.py
@@ -7,8 +7,11 @@ def path_same_directory(path_file: str, filename: str) -> str:
 
     It is often convenient to call with :const:`__file__` as the first parameter, to locate a file on the file-system.
 
-    :param path_file: a path to the file that exists in some directory.
-    :param filename: the filename to assign in the outputted path.
-    :returns: a path to a file with the directory of :code:`path_file` with `filename` appended.
+    Args:
+        path_file: a path to the file that exists in some directory.
+        filename: the filename to assign in the outputted path.
+
+    Returns:
+        a path to a file with the directory of :code:`path_file` with `filename` appended.
     """
     return os.path.join(os.path.dirname(path_file), filename)
